c8390b06212c5ed03404d815dcd8d9cd2181414c
brunoimbrizi <bruno.imbrizi@gmail.com>
2020-10-31 16:50:35 +0000

    Add demo files

--- demo.js
+++
@@ -1,5 +0,0 @@
-const polyparse = require('./index.js');
-
-polyparse('./A.poly', { download: true })
-	.then(data => console.log(data))
-	.catch(err => console.log('ERROR', err))
---
+++ demo/demo.js
@@ -0,0 +1,73 @@
+const polyparse = require('../index.js');
+const normalize = require('normalize-path-scale');
+
+// init canvas
+const canvas = document.querySelector('canvas');
+canvas.width = 640;
+canvas.height = 640;
+
+const ctx = canvas.getContext('2d');
+
+const scale = (points, sx, sy) => {
+	for (let i = 0; i < points.length; i++) {
+		points[i][0] *= sx;
+		points[i][1] *= sy;
+	}
+};
+
+const draw = (data) => {
+	const points = data.pointlist.concat();
+	const segments = data.segmentlist;
+
+	normalize(points);
+	scale(points, canvas.width * 0.4, canvas.height * -0.4);
+
+	ctx.fillStyle = '#eee';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	ctx.save();
+	ctx.translate(canvas.width * 0.5, canvas.height * 0.5);
+	ctx.fillStyle = '#000';
+	
+	// draw points
+  let x, y;
+  for (let i = 0; i < points.length; i++) {
+    x = points[i][0];
+    y = points[i][1];
+    ctx.beginPath();
+    ctx.arc(x, y, 2, 0, Math.PI * 2);
+    ctx.fill();
+  }
+
+
+  // draw segments
+  if (segments) {
+	  let a, b;
+	  for (let i = 0; i < segments.length; i++) {
+	  	a = segments[i][0];
+	    b = segments[i][1];
+
+		  ctx.beginPath();
+		  ctx.moveTo(points[a][0], points[a][1]);
+	    ctx.lineTo(points[b][0], points[b][1]);
+	  	ctx.stroke();
+	  }
+	}
+
+  ctx.restore();
+};
+
+const poly = './A.poly';
+// const poly = './guitar.poly';
+// const poly = './la.poly';
+// const poly = './airfoil_exterior.poly';
+// const poly = './square_circle_hole.poly';
+// const poly = './spiral.node';
+
+fetch(poly)
+	.then(result => result.text())
+	.then(result => {
+		const data = polyparse(result);
+		console.log(data);
+		draw(data);
+	});
